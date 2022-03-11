e2869a98e1c4650ce49b1c0d8b1e5663fa2082c3
Shreeyash Pandey <shreeyashntl@gmail.com>
2022-03-11 22:28:39 +0530

    first Commit

---
+++ ShopKaro/src/main/java/com/shrey/ShopKaro/ShopKaroApplication.java
@@ -0,0 +1,13 @@
+package com.shrey.ShopKaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopKaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopKaroApplication.class, args);
+	}
+
+}
---
+++ ShopKaro/src/test/java/com/shrey/ShopKaro/ShopKaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shrey.ShopKaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopKaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
